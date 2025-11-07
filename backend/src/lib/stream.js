ea6c519a4b61ccf650a9f3926ab69c9ab07e582a
Amit Sahani <95041447+deadpool1718@users.noreply.github.com>
2025-11-07 21:16:25 +0530

    streamapikey_corrected

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,7 +1,7 @@
 import { StreamChat } from "stream-chat";
 import { ENV } from "./env.js";
 
-const apiKey = ENV.STREAM_API_SECRET;
+const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
 
 if (!apiKey || !apiSecret) {
