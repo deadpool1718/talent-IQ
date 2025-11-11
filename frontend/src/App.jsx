c516ba097bc7a630fcf16da0ec6349a47e11cbad
Amit Sahani <95041447+deadpool1718@users.noreply.github.com>
2025-11-12 00:25:32 +0530

    problems page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import {
 import { Routes, Route, Navigate } from "react-router";
 
 import HomePage from "./pages/HomePage";
-import ProblmesPage from "./pages/ProblmesPage";
+import ProblemsPage from "./pages/ProblemsPage";
 import DashboardPage from "./pages/DashboardPage";
 import { Toaster } from "react-hot-toast";
 
@@ -37,7 +37,7 @@ function App() {
 
         <Route
           path="/problems"
-          element={isSignedIn ? <ProblmesPage /> : <Navigate to={"/"} />}
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
         />
       </Routes>
       <Toaster toastOptions={{ duration: 3000 }} />
